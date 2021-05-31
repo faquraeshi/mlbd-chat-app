830e046f597abfdfcd9a2e2d990db0456c812c1e
azmul-ml <azmul.hossain@monstar-lab.com.bd>
2021-05-31 09:12:25 +0600

    Branch created for ant design

---
+++ src/AntDesign/AntDesign.tsx
@@ -0,0 +1,11 @@
+import React, { memo } from "react";
+
+const AntDesignScreen = () => {
+  return (
+    <>
+      <p>Ant Design</p>
+    </>
+  );
+};
+
+export default memo(AntDesignScreen);
---
+++ src/routers/private/router.antdesign.tsx
@@ -0,0 +1,14 @@
+import AntDesign from "AntDesign/AntDesign";
+import { IMenuItemType } from "types/routerType";
+
+/**
+ * Ant Design Screen
+ */
+
+export const ANT_DESIGN_SCREEN: IMenuItemType = {
+  id: "antdesign",
+  path: "/ant/design",
+  component: AntDesign,
+  title: "Ant Design",
+  icon: "appstore",
+};
