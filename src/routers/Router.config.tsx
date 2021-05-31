58f5852dc148e9c14f5c14bb7566ba104565c3f8
azmul-ml <azmul.hossain@monstar-lab.com.bd>
2021-05-31 09:12:25 +0600

    Branch created for ant design

--- src/routers/Router.config.tsx
+++ src/routers/Router.config.tsx
@@ -1,17 +1,20 @@
 import * as DASHBOARD_ROUTES from "./private/router.dashboard";
 import * as CHAT_RECORDS_ROUTES from "./private/router.chatrecords";
 import * as CHAT_TYPES_ROUTES from "./private/router.chattypes";
+import * as ANT_DESIGN_TYPES_ROUTES from "./private/router.antdesign";
 
 /** Private Routes */
 export const PRIVATE_ROUTES = {
   ...DASHBOARD_ROUTES,
   ...CHAT_RECORDS_ROUTES,
-  ...CHAT_TYPES_ROUTES
+  ...CHAT_TYPES_ROUTES,
+  ...ANT_DESIGN_TYPES_ROUTES,
 };
 
 /** Side Navigation Items */
 export const NAVIGATION = [
   DASHBOARD_ROUTES.DASHBOARD_SCREEN,
   CHAT_RECORDS_ROUTES.CHAT_RECORDS_SCREEN,
-  CHAT_TYPES_ROUTES.CHAT_TYPES_SCREEN
+  CHAT_TYPES_ROUTES.CHAT_TYPES_SCREEN,
+  ANT_DESIGN_TYPES_ROUTES.ANT_DESIGN_SCREEN,
 ];
